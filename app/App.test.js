15645210f0f865ecfaa72b20bb99314d2396e37c
wscotten <wscotten@gmail.com>
2018-01-24 18:18:05 -0800

    made test pass

--- app/App.test.js
+++ app/App.test.js
@@ -5,5 +5,5 @@ import renderer from 'react-test-renderer';
 
 it('renders without crashing', () => {
   const rendered = renderer.create(<App />).toJSON();
-  expect(rendered).toBe(false);
+  expect(rendered).toBeTruthy();
 });
