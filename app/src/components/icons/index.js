1d1f6b4092e7435f63d615d4b7c9185946e24a9d
wscotten <wscotten@gmail.com>
2018-02-11 17:08:43 -0800

    more fixes

---
+++ app/src/components/icons/index.js
@@ -0,0 +1,9 @@
+import feedTabIcon from './feedTabIcon';
+import profileTabIcon from './profileTabIcon';
+import recordTabIcon from './recordTabIcon';
+
+export {
+  feedTabIcon,
+  profileTabIcon,
+  recordTabIcon,
+};
---
+++ app/src/components/icons/profileTabIcon.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { MaterialIcons } from 'react-native-vector-icons';
+
+export default function profileTabIcon({ tintColor, focused }) {
+  return (
+    <MaterialIcons
+      name={focused ? 'person' : 'person-outline'}
+      size={26}
+      style={{ color: tintColor }}
+    />
+  );
+}
+
+profileTabIcon.propTypes = {
+  focused: PropTypes.bool.isRequired,
+  tintColor: PropTypes.string.isRequired,
+};
